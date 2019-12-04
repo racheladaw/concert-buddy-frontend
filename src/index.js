6a318cc2717f8b8eff35888dc874a54ce03b4595
Rachel <williamsrachelada@gmail.com>
2019-12-04 13:07:13 -0800

    Moved reducer function to new reducers file and imported to index.js and created a folder for future actions

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import usersReducer from './reducers/users.js';
 
-const users = () => []
+
 const reducer = combineReducers({
-  users: users
+  users: usersReducer
 })
 // never version of how middleware is put together
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
