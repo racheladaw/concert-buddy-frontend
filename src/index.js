45796cd9669807ff28a9dacf51c7dd0d69f5aab3
Rachel <williamsrachelada@gmail.com>
2019-12-04 13:11:42 -0800

    Moved store to its own file to organize index.js further

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import usersReducer from './reducers/users.js';
-
-
-const reducer = combineReducers({
-  users: usersReducer
-})
-// never version of how middleware is put together
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+import store from './store.js';
 
 ReactDOM.render(
   <Provider store={ store }>
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import usersReducer from './reducers/users.js';
+
+
+const reducer = combineReducers({
+  users: usersReducer
+})
+// newer version of how middleware is put together
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
