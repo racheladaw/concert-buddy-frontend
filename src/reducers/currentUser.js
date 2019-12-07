9b9983e2efc7e554bf3afc23c8e42e92f8f988a1
Rachel <williamsrachelada@gmail.com>
2019-12-06 18:13:52 -0800

    Added case for removing current user from store

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,6 +2,8 @@ export default (state = null , action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return action.user
+    case "REMOVE_CURRENT_USER":
+      return null
     default:
       return state
   }
