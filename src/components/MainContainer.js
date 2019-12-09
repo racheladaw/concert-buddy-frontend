65c5ae07324b6422c7bc3a5b1dee8f8662a0161f
Rachel <williamsrachelada@gmail.com>
2019-12-09 15:34:17 -0800

    Added main container component to hold majority of app

---
+++ src/components/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainContainer extends React.Component {
+  render() {
+    return (
+      <div className="MainContainer">
+        <p>This is the Main Container</p>
+      </div>
+    )
+  }
+}
+
+export default MainContainer;
