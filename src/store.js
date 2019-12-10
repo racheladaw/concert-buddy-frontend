ab53ce99e3e8c903b5eb57e2469fdaeadff179fc
Rachel <williamsrachelada@gmail.com>
2019-12-10 13:28:55 -0800

    Added upcomingConcerts reducer to store and combine reducers function

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './reducers/users.js';
 import currentUserReducer from './reducers/currentUser.js';
+import upcomingConcertsReducer from './reducers/upcomingConcerts.js';
 
 
 const reducer = combineReducers({
   users: usersReducer,
-  currentUser: currentUserReducer
+  currentUser: currentUserReducer,
+  upcomingConcerts: upcomingConcertsReducer
 })
 // newer version of how middleware is put together
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
