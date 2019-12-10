b9befc548c0f81a871294a34eb2763636256ab24
Rachel <williamsrachelada@gmail.com>
2019-12-10 14:24:14 -0800

    Mapped state to props to get concert data and created method to render concert cards and pass data

--- src/components/UpcomingConcerts.js
+++ src/components/UpcomingConcerts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getUpcomingConcerts } from '../actions/upcomingConcerts.js';
+import ConcertCard from './ConcertCard';
 
 class UpcomingConcerts extends React.Component {
 
@@ -8,13 +9,26 @@ class UpcomingConcerts extends React.Component {
     this.props.getUpcomingConcerts()
   }
 
+  renderConcertCard() {
+    return this.props.concerts.map(concert_data => {
+      return <ConcertCard key={concert_data.id} concert={concert_data} />
+    })
+  }
+
   render() {
     return (
       <div className="UpcomingConcerts">
         <h1>UpcomingConcerts</h1>
+        {this.renderConcertCard()}
       </div>
     )
   }
 }
 
-export default connect(null, { getUpcomingConcerts })(UpcomingConcerts);
+const mapStateToProps = state => {
+  return {
+    concerts: state.upcomingConcerts
+  }
+}
+
+export default connect(mapStateToProps, { getUpcomingConcerts })(UpcomingConcerts);
