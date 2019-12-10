8192ccd89e3bafc4ff4d1c891ee11dbd3ef66fb0
Rachel <williamsrachelada@gmail.com>
2019-12-10 13:18:41 -0800

    Now imports and renders upcoming concerts component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import UpcomingConcerts from './UpcomingConcerts';
 
 class MainContainer extends React.Component {
   render() {
     return (
       <div className="MainContainer">
         <p>This is the Main Container</p>
+        <UpcomingConcerts />
       </div>
     )
   }
