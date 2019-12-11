85f7c006491405eb0fdf85164aabbbc7c28c116b
Rachel <williamsrachelada@gmail.com>
2019-12-10 19:00:31 -0800

    Mapped state to props to view current user and conditionally render login form and image or upcoming concerts

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import UpcomingConcerts from './UpcomingConcerts';
+import Logo from './Logo';
+import Login from './Login'
+import { connect } from 'react-redux';
 
 class MainContainer extends React.Component {
   render() {
     return (
       <div className="MainContainer">
-        <p>This is the Main Container</p>
-        <UpcomingConcerts />
+        {this.props.currentUser ? (
+          <UpcomingConcerts />
+        ) : (
+          <div className="LoggedOutScreen">
+            <Logo />
+            <Login />
+          </div>
+       )}
       </div>
     )
   }
 }
 
-export default MainContainer;
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(MainContainer);
