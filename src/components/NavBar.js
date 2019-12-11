bcd6fb4c94d607ee42b563a0c5cabfd12de53b32
Rachel <williamsrachelada@gmail.com>
2019-12-10 19:01:21 -0800

    Removed login form from navbar and moved to render conditionally on main container

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Login from './Login';
 import Logout from './Logout';
 
 const NavBar = (props) => {
   return (
     <div className="NavBar">
-      { props.currentUser ? <Logout /> : <Login />}
+      <Logout />
     </div>
   )
 }
