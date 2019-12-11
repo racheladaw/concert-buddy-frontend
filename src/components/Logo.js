a60775c178aac4541d84af4aee77f46516985f72
Rachel <williamsrachelada@gmail.com>
2019-12-10 18:57:45 -0800

    Created Logo component to render image

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/group-of-people-1587927.jpg';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src={logo} alt="Group of people"></img>
+    </div>
+  )
+}
+
+
+export default Logo;
