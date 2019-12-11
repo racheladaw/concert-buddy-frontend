5d88c001ed4ff17c8db81d989662a68d5da55465
Rachel <williamsrachelada@gmail.com>
2019-12-11 15:35:58 -0800

    Added login and sign up routes to component as well as buttons for those routes

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,9 @@ import React from 'react';
 import UpcomingConcerts from './UpcomingConcerts';
 import Logo from './Logo';
 import Login from './Login'
+import SignUp from './SignUp';
 import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 
 class MainContainer extends React.Component {
   render() {
@@ -13,7 +15,11 @@ class MainContainer extends React.Component {
         ) : (
           <div className="LoggedOutScreen">
             <Logo />
-            <Login />
+            <button>Login</button>
+            <p>OR</p>
+            <button>Sign Up</button>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/signup' component={SignUp} />
           </div>
        )}
       </div>
