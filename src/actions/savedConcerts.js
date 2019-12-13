ecbb9368109f73517c99842a46aad82d0bfe8938
Rachel <williamsrachelada@gmail.com>
2019-12-12 22:52:37 -0800

    Added add concert action creator to add saved concert to redux store

--- src/actions/savedConcerts.js
+++ src/actions/savedConcerts.js
@@ -1,3 +1,10 @@
+export const addConcert = saved_concert => {
+  return {
+    type: "ADD_SAVED_CONCERT",
+    concert_id: saved_concert.concert_id
+  }
+}
+
 
 //async
 export const saveConcert = (concert_id, user_id) => {
@@ -18,14 +25,14 @@ export const saveConcert = (concert_id, user_id) => {
 
     return fetch("http://localhost:3001/api/v1/save_concert", configurationObject)
       .then(r => r.json())
-      .then(response => {
-        if (response.error) {
-          alert(response.error)
+      .then(saved_concert => {
+        if (saved_concert.error) {
+          alert(saved_concert.error)
         } else {
-          console.log(response)
+          console.log(saved_concert)
           alert("Concert Saved")
           // need to add to saved concerts redux store
-          // dispatch()
+          dispatch(addConcert(saved_concert))
           // routeHistory.push('/')
         }
       })
