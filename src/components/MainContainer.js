67479509ff04e88853afbac94cf725ff75c9b046
Rachel <williamsrachelada@gmail.com>
2019-12-12 16:01:34 -0800

    Moved Login and Sign up routes and links to welcome component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import UpcomingConcerts from './UpcomingConcerts';
-import Logo from './Logo';
-import Login from './Login'
-import SignUp from './SignUp';
-import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
 
-class MainContainer extends React.Component {
-  render() {
+
+const MainContainer = () => {
     return (
       <div className="MainContainer">
-        {this.props.currentUser ? (
           <UpcomingConcerts />
-        ) : (
-          <div className="LoggedOutScreen">
-            <Logo />
-            <button>Login</button>
-            <p>OR</p>
-            <button>Sign Up</button>
-            <Route exact path='/login' component={Login}/>
-            <Route exact path='/signup' component={SignUp} />
-          </div>
-       )}
       </div>
     )
-  }
 }
 
-const mapStateToProps = state => {
-  return {
-    currentUser: state.currentUser
-  }
-}
-
-export default connect(mapStateToProps)(MainContainer);
+export default MainContainer;
