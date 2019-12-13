8ed54ef1c9b41b67befd73363bceb1d5b53c7693
Rachel <williamsrachelada@gmail.com>
2019-12-12 22:27:10 -0800

    Mapped current user state to props, mapped save concert action creator to component, added button to save concert, changed list elements to p elements

--- src/components/ConcertCard.js
+++ src/components/ConcertCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { saveConcert } from '../actions/savedConcerts.js';
 
 // params are destructured as they are passed as argument
-const ConcertCard = ({ concert }) => {
+const ConcertCard = ({ concert, saveConcert, currentUserId }) => {
 
   return (
     <div className="ConcertCard">
-      <ul>
-        <h4>{concert.attributes.artist_name}</h4>
-        <li>{formatDate(concert.attributes.date)}</li>
-        <li>${concert.attributes.price_range}</li>
-        <li>{concert.attributes.location_json.city}, {concert.attributes.location_json.state}</li>
-      </ul>
+      <h4>{concert.attributes.artist_name}</h4>
+      <p>{formatDate(concert.attributes.date)}</p>
+      <p>${concert.attributes.price_range}</p>
+      <p>{concert.attributes.location_json.city}, {concert.attributes.location_json.state}</p>
+      <button onClick={() => saveConcert(concert.id, currentUserId)}>Save This Concert</button>
     </div>
   )
 }
@@ -20,4 +21,10 @@ const formatDate = date => {
   return formatted_date.substring(0, 10) + ',' + formatted_date.substring(10)
 }
 
-export default ConcertCard;
+const mapStateToProps = state => {
+  return {
+    currentUserId: state.currentUser.id
+  }
+}
+
+export default connect(mapStateToProps, { saveConcert })(ConcertCard);
