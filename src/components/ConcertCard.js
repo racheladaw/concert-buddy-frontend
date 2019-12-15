718e286459df3259e882836db95fa7b2808bc2a6
Rachel <williamsrachelada@gmail.com>
2019-12-14 18:37:01 -0800

    Changed date to render as plain date instead of with format date method

--- src/components/ConcertCard.js
+++ src/components/ConcertCard.js
@@ -8,7 +8,7 @@ const ConcertCard = ({ concert, saveConcert, currentUserId }) => {
   return (
     <div className="ConcertCard">
       <h4>{concert.attributes.artist_name}</h4>
-      <p>{formatDate(concert.attributes.date)}</p>
+      <p>{concert.attributes.date}</p>
       <p>${concert.attributes.price_range}</p>
       <p>{concert.attributes.location_json.city}, {concert.attributes.location_json.state}</p>
       <button onClick={() => saveConcert(concert.id, currentUserId)}>Save This Concert</button>
@@ -16,10 +16,11 @@ const ConcertCard = ({ concert, saveConcert, currentUserId }) => {
   )
 }
 
-const formatDate = date => {
-  const formatted_date = new Date(date).toDateString();
-  return formatted_date.substring(0, 10) + ',' + formatted_date.substring(10)
-}
+//this method is changing the date
+// const formatDate = date => {
+//   const formatted_date = new Date(date).toDateString();
+//   return formatted_date.substring(0, 10) + ',' + formatted_date.substring(10)
+// }
 
 const mapStateToProps = state => {
   return {
