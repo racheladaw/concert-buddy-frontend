cce81b23bbeeab48916008c300eff46d116e62e5
Rachel <williamsrachelada@gmail.com>
2019-12-16 12:47:29 -0800

    Added links to view upcoming concerts and saved concerts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Logout from './Logout';
+import { Link } from 'react-router-dom';
 
 const NavBar = (props) => {
   return (
     <div className="NavBar">
       <Logout />
+      <Link to='/upcoming_concerts' className="concerts-link">Upcoming Concerts</Link><br/>
+      <Link to='/saved_concerts' className="concerts-link">Saved Concerts</Link>
     </div>
   )
 }
