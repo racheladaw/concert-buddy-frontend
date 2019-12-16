b21aa8492da67defbfa6484b93014208d267cd4f
Rachel <williamsrachelada@gmail.com>
2019-12-15 17:22:44 -0800

    Added styling to signup and login links and changed postition of image

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,20 +2,19 @@ import React from 'react';
 import Logo from './Logo';
 import Login from './Login'
 import SignUp from './SignUp'
-import { Switch, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <div className="Welcome">
-      <Logo />
       <h4>Welcome to Concert Buddy</h4>
-      <Link to='/login'>Login</Link><br/>
-      <Link to='/signup'>Sign Up</Link>
+      <Link to='/login' className="welcome-link">Login</Link><br/>
+      <Link to='/signup' className="welcome-link">Sign Up</Link>
       <Switch>
         <Route exact path='/login' component={Login}/>
         <Route exact path='/signup' component={SignUp} />
       </Switch>
+      <Logo />
     </div>
   )
 }
