679ef32b4c7f69759cf41197b3da26b2b4f72ece
Rachel <williamsrachelada@gmail.com>
2019-12-16 13:54:15 -0800

    Added route to render upcoming concerts as default when logging in

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -8,6 +8,7 @@ const MainContainer = () => {
     return (
       <div className="MainContainer">
         <Switch>
+          <Route exact path="/" component={UpcomingConcerts} />
           <Route exact path='/upcoming_concerts' component={UpcomingConcerts}/>
           <Route exact path='/saved_concerts' component={SavedConcerts} />
         </Switch>
