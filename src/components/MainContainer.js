8616565f33afbb7fe4b104068e64080eb5f2579f
Rachel <williamsrachelada@gmail.com>
2019-12-16 12:36:38 -0800

    Rendered saved concerts component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import UpcomingConcerts from './UpcomingConcerts';
+import SavedConcerts from './SavedConcerts';
 
 
 const MainContainer = () => {
     return (
       <div className="MainContainer">
           <UpcomingConcerts />
+          <SavedConcerts />
       </div>
     )
 }
