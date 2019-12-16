dfbb838097b479cf0742cfeb5b42d54c6341943f
Rachel <williamsrachelada@gmail.com>
2019-12-16 13:54:58 -0800

    Added welcome, user list element

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,20 @@ import { connect } from 'react-redux';
 import Logout from './Logout';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = (props) => {
+const NavBar = ({ currentUserName }) => {
   return (
     <ul className="NavBar">
       <li><NavLink to='/upcoming_concerts' activeClassName="active" className="concerts-link">Upcoming Concerts</NavLink><br/></li>
       <li><NavLink to='/saved_concerts' activeClassName="active" className="concerts-link">Saved Concerts</NavLink></li>
       <li><Logout /></li>
+      <li id="welcome-user">Welcome, {currentUserName}</li>
     </ul>
   )
 }
 
 const mapStateToProps = state => {
   return {
-    currentUser: state.currentUser
+    currentUserName: state.currentUser.attributes.name
   }
 }
 
