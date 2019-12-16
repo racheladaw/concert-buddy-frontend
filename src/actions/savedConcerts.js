6bdb7816ba1faa85a23805819a277df1bb060d4b
Rachel <williamsrachelada@gmail.com>
2019-12-16 12:39:26 -0800

    Action creator getSavedConcerts created to fetch saved concert data and add to store. saveConcert action creator changed to dispatch concert object instead of concert id to add concert action creator. Add concert action creator changed to accept new data.

--- src/actions/savedConcerts.js
+++ src/actions/savedConcerts.js
@@ -1,7 +1,14 @@
-export const addConcert = saved_concert => {
+export const addConcert = data => {
   return {
     type: "ADD_SAVED_CONCERT",
-    concert_id: saved_concert.concert_id
+    concert: data
+  }
+}
+
+export const setSavedConcerts = data => {
+  return {
+    type: "SET_SAVED_CONCERTS",
+    concerts: data
   }
 }
 
@@ -31,10 +38,28 @@ export const saveConcert = (concert_id, user_id) => {
         } else {
           console.log(saved_concert)
           alert("Concert Saved")
-          // need to add to saved concerts redux store
-          dispatch(addConcert(saved_concert))
+          dispatch(addConcert(saved_concert.data))
           // routeHistory.push('/')
         }
+      })
+      .catch(error => console.log(error))
+  }
+}
+
+export const getSavedConcerts = () => {
+  return dispatch => {
+    const configurationObject = {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    return fetch("http://localhost:3001/api/v1/saved_concerts", configurationObject)
+      .then(r => r.json())
+      .then(saved_concerts => {
+        dispatch(setSavedConcerts(saved_concerts.data))
       })
       .catch(error => console.log(error))
   }
