ae31d93b7b59e84828ec18f9ac599f8835444b0e
Rachel <williamsrachelada@gmail.com>
2019-12-28 13:49:16 -0800

    Changed NavLinks to go to home/upcoming_concerts and home/saved_concerts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ import { NavLink } from 'react-router-dom';
 const NavBar = ({ currentUserName }) => {
   return (
     <ul className="NavBar">
-      <li><NavLink to='/upcoming_concerts' activeClassName="active" className="concerts-link">Upcoming Concerts</NavLink><br/></li>
-      <li><NavLink to='/saved_concerts' activeClassName="active" className="concerts-link">Saved Concerts</NavLink></li>
+      <li><NavLink to='/home/upcoming_concerts' activeClassName="active" className="concerts-link">Upcoming Concerts</NavLink><br/></li>
+      <li><NavLink to='/home/saved_concerts' activeClassName="active" className="concerts-link">Saved Concerts</NavLink></li>
       <li><Logout /></li>
       <li id="welcome-user">Welcome, {currentUserName}</li>
     </ul>
