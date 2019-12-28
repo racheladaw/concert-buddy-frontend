0cab680d59777cbcc82c193b1978fde533169e2b
Rachel <williamsrachelada@gmail.com>
2019-12-28 13:45:40 -0800

    wrapped component in withRouter to pass route history as parameter to logout action creator

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions/currentUser.js';
+import { withRouter } from 'react-router';
 
 // params are destructured as they are passed as argument
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history }) => {
   return (
     <div>
-      <form onSubmit={logout}>
+      <form onSubmit={() => logout(history)}>
         <input className="logout-button" type="submit" value="Log Out"/>
       </form>
     </div>
@@ -14,4 +15,4 @@ const Logout = ({ logout }) => {
 }
 
 
-export default connect(null, { logout })(Logout);
+export default connect(null, { logout })(withRouter(Logout));
