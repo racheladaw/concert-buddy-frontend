e9ac8b6630af5c6b288ca192a061fb3b447db2bb
Rachel <williamsrachelada@gmail.com>
2019-12-28 13:44:47 -0800

    Changed concertCard to class component and created upvote button functionality

--- src/components/ConcertCard.js
+++ src/components/ConcertCard.js
@@ -3,23 +3,41 @@ import { connect } from 'react-redux';
 import { saveConcert } from '../actions/savedConcerts.js';
 
 // params are destructured as they are passed as argument
-const ConcertCard = ({ concert, saveConcert, currentUserId, saved_concert }) => {
+// ({ concert, saveConcert, currentUserId, saved_concert })
+class ConcertCard extends React.Component {
+  constructor() {
+    super();
 
-  const renderButton = () => {
-    if (!saved_concert) {
-      return <button onClick={() => saveConcert(concert.id, currentUserId)}>Save This Concert</button>
+    this.state = {
+      upvote: 0
     }
   }
 
+  renderButton = () => {
+    if (!this.props.saved_concert) {
+      return <button onClick={() => this.props.saveConcert(this.props.concert.id, this.props.currentUserId)}>Save This Concert</button>
+    }
+  }
+
+  handleUpvote = () => {
+    let num = this.state.upvote + 1
+    this.setState({
+      upvote: num
+    })
+  }
+
+  render() {
   return (
     <div className="ConcertCard">
-      <h4>{concert.attributes.artist_name}</h4>
-      <p>{concert.attributes.date}</p>
-      <p>${concert.attributes.price_range}</p>
-      <p>{concert.attributes.location_json.city}, {concert.attributes.location_json.state}</p>
-      {renderButton()}
+      <h4>{this.props.concert.attributes.artist_name}</h4>
+      <p>{this.props.concert.attributes.date}</p>
+      <p>${this.props.concert.attributes.price_range}</p>
+      <p>{this.props.concert.attributes.location_json.city}, {this.props.concert.attributes.location_json.state}</p>
+      <button onClick={this.handleUpvote}>Upvote this: {this.state.upvote}</button>
+      {this.renderButton()}
     </div>
-  )
+    )
+  }
 }
 
 //this method is changing the date
