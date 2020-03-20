ac3e9f298b83ba6b988051b61f51db59579e1b56
Rachel <williamsrachelada@gmail.com>
2020-03-20 13:12:49 -0700

    Added button for user to upload a profile picture. Added renderProfilePicture method to display users profile picture or a blank avatar photo

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import blankProfilePicture from '../images/Portrait_Placeholder.png';
 
 const Profile = ( props ) => {
+
+  const renderProfilePicture = () => {
+    if (props.profilePicture) {
+      return <img alt="Profile picture"></img>
+    } else {
+      return <img id="blankProfilePicture" src={blankProfilePicture} alt="Blank profile avatar"></img>
+    }
+  }
 
   return (
     <div className="Profile">
       <h1>My Profile</h1>
+      {renderProfilePicture()}
       <h5>{props.name}</h5>
       <h5>{props.city}, {props.stateName}</h5>
       <h5>Username: {props.userName}</h5>
+
+      <button>Upload a Profile Picture</button>
     </div>
   )
 }
