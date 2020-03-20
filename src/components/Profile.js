5b59e2fe7b93d10323093ebf9b935d5c87b46d01
Rachel <williamsrachelada@gmail.com>
2020-03-20 12:59:45 -0700

    Mapped state to props to display current user's data on profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,8 +6,20 @@ const Profile = ( props ) => {
   return (
     <div className="Profile">
       <h1>My Profile</h1>
+      <h5>{props.name}</h5>
+      <h5>{props.city}, {props.stateName}</h5>
+      <h5>Username: {props.userName}</h5>
     </div>
   )
 }
 
-export default Profile;
+const mapStateToProps = state => {
+  return {
+    userName: state.currentUser.attributes.username,
+    name: state.currentUser.attributes.name,
+    city: state.currentUser.attributes.location_json.city,
+    stateName: state.currentUser.attributes.location_json.state
+  }
+}
+
+export default connect(mapStateToProps)(Profile);
