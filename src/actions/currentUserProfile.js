76a5134e9fe942c3e21785294af39ca59a564c74
Rachel <williamsrachelada@gmail.com>
2020-03-29 18:29:29 -0700

    Imported baseUrl and added action creator to send post request for profile image upload to rails API

--- src/actions/currentUserProfile.js
+++ src/actions/currentUserProfile.js
@@ -1,5 +1,29 @@
+import { baseUrl } from '../config/baseUrl';
+
 export const setFormStateToActive = () => {
   return {
     type: "SHOW_FORM"
+  }
+}
+
+export const uploadProfilePicture = (formData, userId) => {
+  console.log("hit action creator", userId)
+  return dispatch => {
+    const configurationObject = {
+      credentials: "include",
+      method: "POST",
+      body: formData
+    }
+
+    return fetch(`${baseUrl}/api/v1/users/${userId}/upload_photo`, configurationObject)
+      .then(r => r.json())
+      .then(photo => {
+        if (photo.error) {
+          alert(photo.error)
+        } else {
+          console.log("success")
+        }
+      })
+      .catch(error => console.log(error))
   }
 }
