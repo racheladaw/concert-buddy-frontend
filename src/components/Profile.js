9ce60911271cbefac4526fa2c363d87ddf9c1919
Rachel <williamsrachelada@gmail.com>
2020-03-29 18:32:03 -0700

    Imported action creators to toggle form and send post request with profile photo. Added submitPhoto method to format formData and send to action creator function.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import blankProfilePicture from '../images/Portrait_Placeholder.png';
-import { setFormStateToActive } from '../actions/currentUserProfile.js'
+import { setFormStateToActive } from '../actions/currentUserProfile.js';
+import { uploadProfilePicture } from '../actions/currentUserProfile.js';
 
 const Profile = ( props ) => {
 
@@ -11,6 +12,18 @@ const Profile = ( props ) => {
     } else {
       return <img id="blankProfilePicture" src={blankProfilePicture} alt="Blank profile avatar"></img>
     }
+  }
+
+  const submitPhoto = (event) => {
+    event.preventDefault();
+    let photoInput = document.getElementById('profile-photo-input');
+    if (photoInput.files[0]) {
+      const formData = new FormData();
+      const upload_file = photoInput.files[0]
+      formData.append('file', upload_file);
+      props.submitProfilePhoto(formData, props.userId)
+    }
+
   }
 
   return (
@@ -23,11 +36,15 @@ const Profile = ( props ) => {
 
       <button onClick={props.toggleForm} className={props.buttonStatus === "Active" ? "" : "hidden"}>Upload a Profile Photo</button>
 
-      <form className={props.formStatus === "Active" ? "" : "hidden"}>
+      <form className={props.formStatus === "Active" ? "" : "hidden"} onSubmit={submitPhoto}>
+        <input type="file"
+          id="profile-photo-input" name="profile-photo"
+          accept="image/png, image/jpeg"
+        />
         <input
-            className="submit-input"
-            type="submit"
-            value="Upload"
+          className="submit-input"
+          type="submit"
+          value="Upload"
         />
       </form>
     </div>
@@ -37,6 +54,7 @@ const Profile = ( props ) => {
 const mapStateToProps = state => {
   return {
     userName: state.currentUser.attributes.username,
+    userId: state.currentUser.id,
     name: state.currentUser.attributes.name,
     city: state.currentUser.attributes.location_json.city,
     stateName: state.currentUser.attributes.location_json.state,
@@ -47,7 +65,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    toggleForm: () => dispatch(setFormStateToActive())
+    toggleForm: () => dispatch(setFormStateToActive()),
+    submitProfilePhoto: (formData, userId) => dispatch(uploadProfilePicture(formData, userId))
   }
 }
 
