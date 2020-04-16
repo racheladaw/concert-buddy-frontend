42e24aff7dd2c1c3e67cc4fc6c74268baf6bbea5
Rachel <williamsrachelada@gmail.com>
2020-04-16 16:24:05 -0700

    Added SET_USER case to set redux store with user info for profile. Reformated default state to be an object instead of array.

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,12 @@
-export default (state = [] , action) => {
+export default (state = {user: null} , action) => {
   switch (action.type) {
+    case "SET_USER":
+      return {user: {
+        name: action.name,
+        username: action.username,
+        profilePicture: action.profilePicture,
+        location: action.location
+      }}
     default:
       return state
   }
