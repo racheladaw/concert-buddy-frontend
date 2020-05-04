333da5c85ba318fc955ad19caa04c218bb55e411
Rachel <williamsrachelada@gmail.com>
2020-05-04 16:33:38 -0700

    Added styling to keep footer on bottom of page. Changed footer container to footer element instead of div.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <a target="_blank" href="https://icons8.com/icons/set/dj">DJ icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-    </div>
+    </footer>
   )
 }
 
