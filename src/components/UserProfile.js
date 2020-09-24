d88df20b5da894ded31d3a405d012ec10ea222c9
Rachel <williamsrachelada@gmail.com>
2020-09-23 18:05:35 -0700

    Added UserProfile component to display a user's profile page.

---
+++ src/components/UserProfile.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import blankProfilePicture from '../images/Portrait_Placeholder.png';
+import { getUsersProfile } from '../actions/users.js';
+
+const UserProfile = ( props ) => {
+
+  useEffect(() => {
+    console.log("props", props)
+    props.getUsersProfile(props.match.params.id)
+  }, [])
+
+  const renderProfilePicture = () => {
+    console.log("profile pic", props.profilePicture)
+    if (props.profilePicture) {
+      return <img className="profilePicture" alt="Profile picture" src={props.profilePicture}></img>
+    } else {
+      return <img className="profilePicture" src={blankProfilePicture} alt="Blank profile avatar"></img>
+    }
+  }
+
+  return (
+    <div className="Profile">
+      <h1>{props.name}</h1>
+      {renderProfilePicture()}
+      <h5>{props.location}</h5>
+      <h5>Username: {props.userName}</h5>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    userName: state.users.user.username,
+    name: state.users.user.name,
+    location: state.users.user.location,
+    profilePicture: state.users.user.profilePicture
+  }
+}
+
+export default connect(mapStateToProps, { getUsersProfile })(UserProfile);
